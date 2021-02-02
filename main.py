a6daa935d8690869ea4fcb0ebf54c7e5ad4fb843
Abe280 <72824753+Abe280@users.noreply.github.com>
2021-02-02 10:01:36 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 while True:
+    print(input.rotation(Rotation.ROLL))
     if input.rotation(Rotation.ROLL) <45:
         light.set_all(light.rgb(255, 0, 255))
+    else:
+        light.clear()
--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 while (true) {
+    console.log(input.rotation(Rotation.Roll))
     if (input.rotation(Rotation.Roll) < 45) {
         light.setAll(light.rgb(255, 0, 255))
+    } else {
+        light.clear()
     }
     
 }
